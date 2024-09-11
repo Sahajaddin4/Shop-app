3612f3bf8c4892f92b753a2a9c6fc461861bfc09
Wasifkalimkalim <wasifnit2k19@gmail.com>
2024-09-11 19:09:50 +0530

    Cart UI in progress

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -22,7 +22,7 @@ function Home() {
 
 
   return (
-    <div className='max-w-[80%] mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5  mt-12'>
+    <div className='max-w-[80%] mx-auto grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-5   mt-12'>
       
       {
         loader ? <Spinner /> :
--- src/components/pages/cart/CartItem.jsx
+++ src/components/pages/cart/CartItem.jsx
@@ -4,23 +4,23 @@ function CartItem({ item }) {
   let title = item.title;
   //title = title.substring(0, 15) + "...";
   let description=item.description;
-  description=description.substring(0, 50);
+  description=description.substring(0, 100) + "....";
     
   return (
    <>
-        <div className="cart-item flex gap-5">
-             <div className="product-image">
-              <img src={item.image} alt="product-image" className="h-[180px]"/>
+        <div className="cart-item p-2 flex gap-5 border-b-4">
+             <div className="product-image w-[30%] my-auto">
+              <img src={item.image} alt="product-image" className="object-cover"/>
              </div>
-             <div className="product-details flex flex-col gap-5">
+             <div className="product-details flex flex-col space-y-5 w-full">
                 <div className="title">
-                  <h1>{title}</h1>
+                  <h1 className="font-semibold text-lg text-slate-700 ">{title}</h1>
                 </div>
                 <div className="description">
-                    <small>{description}</small>
+                    <small className="text-slate-700 font-medium">{description}</small>
                 </div>
                 <div className="price-and-remove-btn flex justify-between items-center">
-                    <p>${item.price}</p>
+                    <p className="text-green-500 font-bold text-lg">${item.price}</p>
                     <div className="remove-btn rounded-full bg-red-300 p-3 hover:cursor-pointer hover:bg-red-500 hover:text-white text-red-800">
                     <MdDelete />
                     </div>
